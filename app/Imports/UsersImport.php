2d55ef50652e73dc91309826d4a6a8f04844d222
KoreGod <82606871+Kore-afk@users.noreply.github.com>
2024-11-07 13:47:06 -0600

    07112024

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -5,8 +5,12 @@ namespace App\Imports;
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\WithMappedCells;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
-class UsersImport implements ToModel
+
+class UsersImport implements ToModel, WithMappedCells
 {
     /**
     * @param array $row
@@ -16,9 +20,27 @@ class UsersImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            'name'     => $row[0],
-           'email'    => $row[1], 
-           'password' => Hash::make($row[2]),
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'phone' => $row['phone'],
+            'password' => Hash::make($row['password']),
+            'row' => json_encode(['name' => '2', 'email' => '4', 'phone' => '3', 'password' => '5']),
+            'column' => json_encode(['name' => 'C', 'email' => 'C', 'phone' => 'C', 'password' => 'C']),
         ]);
     }
+    public function mapping(): array
+    {
+        return [
+            'name'  => 'C2',
+            'email' => 'C4',
+            'phone' => 'C3',
+            'password' => 'C5',
+        ];
+    }
+    public function collection(array $row)
+    {
+    }
+
+
 }
+
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,16 +2,13 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, useForm } from "@inertiajs/react";
 import { useState } from "react";
 
-export default function Dashboard({ auth,  caratulasConfig }) {
+export default function Dashboard({ auth, caratulasConfig }) {
     // Estado para controlar la visibilidad del modal y reporte seleccionado
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-   
 
     // Inicializa el formulario con useForm, manejando el estado del archivo, empresa y año
     const { data, setData, post, processing, errors, reset } = useForm({
         file: null,
-       
     });
 
     const handleFileChange = (event) => setData("file", event.target.files[0]);
@@ -42,7 +39,11 @@ export default function Dashboard({ auth,  caratulasConfig }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Carga de Archivo</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Carga de Archivo
+                </h2>
+            }
         >
             <Head title="Inicio" />
 
@@ -50,14 +51,8 @@ export default function Dashboard({ auth,  caratulasConfig }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                           
-
                             <form onSubmit={handleSubmit} className="space-y-6">
                                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                                    
-
-                                   
-
                                     <div className="flex flex-col items-start">
                                         <span className="flex items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                             Selecciona un archivo excel
@@ -68,18 +63,24 @@ export default function Dashboard({ auth,  caratulasConfig }) {
                                             type="file"
                                             onChange={handleFileChange}
                                         />
-                                        {errors.file && <div className="text-red-600 mt-2">{errors.file}</div>}
+                                        {errors.file && (
+                                            <div className="text-red-600 mt-2">
+                                                {errors.file}
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
 
                                 <div className="flex justify-end">
-                                    <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded" disabled={processing}>
+                                    <button
+                                        type="submit"
+                                        className="bg-blue-500 text-white py-2 px-4 rounded"
+                                        disabled={processing}
+                                    >
                                         {processing ? "Enviando..." : "Enviar"}
                                     </button>
                                 </div>
                             </form>
-
-                           
                         </div>
                     </div>
                 </div>
